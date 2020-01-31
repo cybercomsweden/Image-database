c469f46099c0529fbd35c6991a3cb6ea140ed823
Christoffer Dahl <chdah1@LPI-5CG9061TZZ.global.ad>
2020-01-31 12:17:37 +0100

    Start to display pictures

--- src/index.jsx
+++ src/index.jsx
@@ -1,59 +1,8 @@
-import api from './entity_pb.js';
+import App from './media.jsx'
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getThumbnailPaths() {
-    const response = fetch("/list").then((response) => {
-        return response.blob();
-    }).then((blob) => {
-        return blob.arrayBuffer();
-    }).then((buf) => {
-        const thumbnailPaths = [];
-        for (let entity of api.Entities.deserializeBinary(buf).getEntityList()) {
-            thumbnailPaths.push(entity.getThumbnailPath());
-        }
-
-        return thumbnailPaths;
-    });
-    return response;
-}
-
-class Media extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            media: []
-        };
-    }
-
-    getThumbnails() {
-        getThumbnailPaths().then((paths) => {
-            const thumbnails = [];
-            var id = 0;
-            for (let p of paths) {
-                let path = `/media/${p.replace(/\\/, "/")}`;
-                thumbnails.push(<div key={id} className="media-thumbnail"><img src={path}/></div>);
-                id = id + 1;
-            }
-
-            this.setState({ media: thumbnails });
-        });
-    }
-
-    componentDidMount() {
-        this.getThumbnails();
-    }
-
-    render() {
-        return (
-            <div className="media-thumbnail-list">
-            {this.state.media}
-            </div>
-        );
-    }
-}
-
 ReactDOM.render(
-    <Media />,
+    <App />,
     document.getElementById('root')
 )
---
+++ src/media.jsx
@@ -0,0 +1,119 @@
+import api from './entity_pb.js';
+import React from 'react';
+import { BrowserRouter, Switch, Route, Link, useParams } from 'react-router-dom'
+
+function getThumbnailPaths() {
+    const response = fetch("/list").then((response) => {
+        return response.blob();
+    }).then((blob) => {
+        return blob.arrayBuffer();
+    }).then((buf) => {
+        const thumbnailPaths = [];
+        for (let entity of api.Entities.deserializeBinary(buf).getEntityList()) {
+            thumbnailPaths.push(entity);
+        }
+
+        return thumbnailPaths;
+    });
+    return response;
+}
+
+class Pic extends React.Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+        this.state = {
+            entity: []
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                {/* TODO: change hardcoded path to picture */}
+                <img src="/media/dest/Thinker-Auguste-Rodin-Museum-Paris-1904.jpg" />
+            </div>
+        );
+    }
+}
+
+class Media extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            media: [],
+            entities: []
+        };
+    }
+
+    getThumbnails() {
+        getThumbnailPaths().then((entities) => {
+            const thumbnails = [];
+            var id = 0;
+            for (let e of entities) {
+                let orig_path = e.getThumbnailPath();
+                console.log(e.getId());
+                let path = `/media/${orig_path.replace(/\\/, "/")}`;
+                let link = `/media/${e.getId()}`
+                thumbnails.push(
+                        <div key={id} className="media-thumbnail">
+                            <Link to={link}><img src={path} /></Link>
+                        </div>)
+                id = id + 1;
+            }
+
+            this.setState({ media: thumbnails, entities: entities });
+            console.log(this.state.entities);
+        });
+    }
+
+    componentDidMount() {
+        this.getThumbnails();
+    }
+
+    render() {
+        return (
+            <div className="media-thumbnail-list">
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/">
+                            {this.state.media}
+                        </Route>
+                        <Route exact path="/media/:id" children={({ match }) => {
+                            return <Pic entity={match.params.id} />;
+                        }} />
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+class Search extends React.Component {
+    render() {
+        return (
+            <div id="search" className="search-bar">
+                <input type="text" name="search"/>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="content">
+                <header>
+                    <nav>
+                        <a className="active" href="/">Media</a>
+                        <a href="/">Tags</a>
+                    </nav>
+                    <Search />
+                </header>
+                <Media />
+            </div>
+        );
+    }
+}
+
+export default App;
