0354be89dc593cb56e848f091d8a4309f6771707
Margareta Vi <margareta.vi@gmail.com>
2020-03-12 08:33:22 +0100

    Revert "Add license" Due to not adding license text in block comment
    This reverts commit 6a974ed74476be5b10582f1bd4f0bbeb0762272f.

--- gui/index.jsx
+++ gui/index.jsx
@@ -1,20 +1,3 @@
-Image database, allows the user to host a database themselves,
-with the possibilities to tag and search after images.
-Copyright (C) 2020 Cybercom group AB, Sweden
-By Christoffer Dahl, Johanna Hultberg, Andreas Runfalk and Margareta Vi
-
-Image database is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <https://www.gnu.org/licenses/>.
 import React from "react";
 import ReactDOM from "react-dom";
 import {
